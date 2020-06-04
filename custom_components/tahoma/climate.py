7f0ec23e92f004d4c7ed60b6063802084e0330fa
Vincent Le Bourlot <vlebourl@gmail.com>
2020-06-04 21:43:36 +0200

    limit the climate integration to SomfyThermostat widget, implemented required properties and methods.

--- custom_components/tahoma/climate.py
+++ custom_components/tahoma/climate.py
@@ -1,12 +1,18 @@
 """Support for Tahoma climate."""
 from datetime import timedelta
 import logging
-from typing import List
+from typing import List, Optional
 
 from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import (
     HVAC_MODE_OFF,
     HVAC_MODE_AUTO,
+    PRESET_AWAY,
+    PRESET_HOME,
+    PRESET_NONE,
+    PRESET_SLEEP,
+    SUPPORT_PRESET_MODE,
+    SUPPORT_TARGET_TEMPERATURE,
 )
 
 from .const import DOMAIN, TAHOMA_TYPES
@@ -15,6 +21,8 @@ from .tahoma_device import TahomaDevice
 _LOGGER = logging.getLogger(__name__)
 
 SCAN_INTERVAL = timedelta(seconds=120)
+
+PRESET_FP = "Frost Protection"
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
@@ -27,9 +35,11 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
     for device in data.get("devices"):
         if TAHOMA_TYPES[device.uiclass] == "climate":
-            entities.append(TahomaClimate(device, controller))
+            if device.widget == "SomfyThermostat":
+                entities.append(TahomaClimate(device, controller))
 
     async_add_entities(entities)
+
 
 class TahomaClimate(TahomaDevice, ClimateEntity):
     """Representation of a Tahoma thermostat."""
@@ -37,13 +47,15 @@ class TahomaClimate(TahomaDevice, ClimateEntity):
     def __init__(self, tahoma_device, controller):
         """Initialize the sensor."""
         super().__init__(tahoma_device, controller)
+        self._preset_modes = [PRESET_AWAY, PRESET_FP, PRESET_HOME, PRESET_NONE, PRESET_SLEEP]
+        self._preset_mode = None
         self._hvac_modes = [HVAC_MODE_OFF, HVAC_MODE_AUTO]
         self._hvac_mode = None
 
     def update(self):
         """Update the state."""
         self.controller.get_states([self.tahoma_device])
-        #TODO implement update method
+        # TODO implement update method
 
     @property
     def hvac_mode(self) -> str:
@@ -60,3 +72,24 @@ class TahomaClimate(TahomaDevice, ClimateEntity):
         Need to be a subset of HVAC_MODES.
         """
         return self._hvac_modes
+
+    @property
+    def supported_features(self) -> int:
+        """Return the list of supported features."""
+        return SUPPORT_PRESET_MODE | SUPPORT_TARGET_TEMPERATURE
+
+    @property
+    def preset_mode(self) -> Optional[str]:
+        """Return the current preset mode, e.g., home, away, temp.
+
+        Requires SUPPORT_PRESET_MODE.
+        """
+        return self._preset_mode
+
+    @property
+    def preset_modes(self) -> Optional[List[str]]:
+        """Return a list of available preset modes.
+
+        Requires SUPPORT_PRESET_MODE.
+        """
+        return self._preset_modes
