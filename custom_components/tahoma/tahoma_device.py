d6b66c468138bc6bd88ce990749a566e265dec57
Vincent Le Bourlot <vlebourl@gmail.com>
2020-06-15 15:11:45 +0200

    Update tahoma_device.py

--- custom_components/tahoma/tahoma_device.py
+++ custom_components/tahoma/tahoma_device.py
@@ -24,10 +24,6 @@ class TahomaDevice(Entity):
         self._name = self.tahoma_device.label
         self.controller = controller
 
-    async def async_added_to_hass(self):
-        await super().async_added_to_hass()
-        self.schedule_update_ha_state(True)
-
     @property
     def name(self):
         """Return the name of the device."""
