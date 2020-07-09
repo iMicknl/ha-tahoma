117fb011b625756f2599621b0a786d61b0b0dc06
vlebourl <vlebourl@gmail.com>
2020-07-09 11:16:20 +0200

    fix/Light update failing.

--- custom_components/tahoma/light.py
+++ custom_components/tahoma/light.py
@@ -144,10 +144,13 @@ class TahomaLight(TahomaDevice, LightEntity):
         states = self.tahoma_device.active_states
 
         self._state = states.get(CORE_ON_OFF_STATE) == STATE_ON
-        self._brightness = states.get(CORE_LIGHT_INTENSITY_STATE)
 
-        self._hs_color = color_util.color_RGB_to_hs(
-            states.get(CORE_RED_COLOR_INTENSITY_STATE),
-            states.get(CORE_GREEN_COLOR_INTENSITY_STATE),
-            states.get(CORE_BLUE_COLOR_INTENSITY_STATE),
-        )
+        if CORE_LIGHT_INTENSITY_STATE in states:
+            self._brightness = states.get(CORE_LIGHT_INTENSITY_STATE)
+
+        if CORE_RED_COLOR_INTENSITY_STATE in states:
+            self._hs_color = color_util.color_RGB_to_hs(
+                states.get(CORE_RED_COLOR_INTENSITY_STATE),
+                states.get(CORE_GREEN_COLOR_INTENSITY_STATE),
+                states.get(CORE_BLUE_COLOR_INTENSITY_STATE),
+            )
