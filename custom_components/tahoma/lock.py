18a61fd13eb89cf5a5ab3e48aedd7b9e1a822e6f
Thibaut Etienne <thibaut@etienne.pw>
2020-07-11 09:09:55 +0200

    Clean lock

--- custom_components/tahoma/lock.py
+++ custom_components/tahoma/lock.py
@@ -17,8 +17,6 @@ COMMAND_UNLOCK = "unlock"
 
 CORE_LOCKED_UNLOCKED_STATE = "core:LockedUnlockedState"
 
-TAHOMA_STATE_LOCKED = "locked"
-
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up the TaHoma locks from a config entry."""
@@ -38,26 +36,6 @@ async def async_setup_entry(hass, entry, async_add_entities):
 class TahomaLock(TahomaDevice, LockEntity):
     """Representation a TaHoma lock."""
 
-    def __init__(self, tahoma_device, controller):
-        """Initialize the device."""
-        super().__init__(tahoma_device, controller)
-        self._lock_status = None
-
-    def update(self):
-        """Update method."""
-        if self.should_wait():
-            self.schedule_update_ha_state(True)
-            return
-
-        self.controller.get_states([self.tahoma_device])
-        if (
-            self.tahoma_device.active_states.get(CORE_LOCKED_UNLOCKED_STATE)
-            == TAHOMA_STATE_LOCKED
-        ):
-            self._lock_status = STATE_LOCKED
-        else:
-            self._lock_status = STATE_UNLOCKED
-
     def unlock(self, **kwargs):
         """Unlock method."""
         self.apply_action(COMMAND_UNLOCK)
@@ -67,11 +45,9 @@ class TahomaLock(TahomaDevice, LockEntity):
         self.apply_action(COMMAND_LOCK)
 
     @property
-    def name(self):
-        """Return the name of the lock."""
-        return self.tahoma_device.active_states["core:NameState"]
-
-    @property
     def is_locked(self):
         """Return True if the lock is locked."""
-        return self._lock_status == STATE_LOCKED
+        return (
+            self.tahoma_device.active_states.get(CORE_LOCKED_UNLOCKED_STATE)
+            == STATE_LOCKED
+        )
