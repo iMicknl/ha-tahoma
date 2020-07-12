8f45830e5f42acc82267126268bc65b525a97971
Thibaut Etienne <thibaut@etienne.pw>
2020-07-13 00:23:08 +0200

    Clean sensor

--- custom_components/tahoma/sensor.py
+++ custom_components/tahoma/sensor.py
@@ -82,16 +82,38 @@ async def async_setup_entry(hass, entry, async_add_entities):
 class TahomaSensor(TahomaDevice, Entity):
     """Representation of a TaHoma Sensor."""
 
-    def __init__(self, tahoma_device, controller):
-        """Initialize the sensor."""
-        self.current_value = None
-
-        super().__init__(tahoma_device, controller)
-
     @property
     def state(self):
         """Return the name of the sensor."""
-        return self.current_value
+
+        states = self.tahoma_device.active_states
+
+        if CORE_LUMINANCE_STATE in states:
+            return states.get(CORE_LUMINANCE_STATE)
+
+        if CORE_RELATIVE_HUMIDITY_STATE in states:
+            return float(states.get(CORE_RELATIVE_HUMIDITY_STATE))
+
+        if CORE_TEMPERATURE_STATE in states:
+            return float(states.get(CORE_TEMPERATURE_STATE))
+
+        if CORE_ELECTRIC_POWER_CONSUMPTION_STATE in states:
+            return float(states.get(CORE_ELECTRIC_POWER_CONSUMPTION_STATE))
+
+        if CORE_ELECTRIC_ENERGY_CONSUMPTION_STATE in states:
+            return float(states.get(CORE_ELECTRIC_ENERGY_CONSUMPTION_STATE)) / 1000
+
+        if CORE_CO_CONCENTRATION_STATE in states:
+            return int(states.get(CORE_CO_CONCENTRATION_STATE))
+
+        if CORE_CO2_CONCENTRATION_STATE in states:
+            return int(states.get(CORE_CO2_CONCENTRATION_STATE))
+
+        if CORE_WINDSPEED_STATE in states:
+            return float("{:.2f}".format(states.get(CORE_WINDSPEED_STATE)))
+
+        if CORE_SUN_ENERGY_STATE in states:
+            return float("{:.2f}".format(states.get(CORE_SUN_ENERGY_STATE)))
 
     @property
     def unit_of_measurement(self):
@@ -149,48 +171,3 @@ class TahomaSensor(TahomaDevice, Entity):
             or TAHOMA_SENSOR_DEVICE_CLASSES.get(self.tahoma_device.uiclass)
             or None
         )
-
-    def update(self):
-        """Update the state."""
-        if self.should_wait():
-            self.schedule_update_ha_state(True)
-            return
-
-        self.controller.get_states([self.tahoma_device])
-
-        states = self.tahoma_device.active_states
-
-        if CORE_LUMINANCE_STATE in states:
-            self.current_value = states.get(CORE_LUMINANCE_STATE)
-
-        if CORE_RELATIVE_HUMIDITY_STATE in states:
-            self.current_value = float(states.get(CORE_RELATIVE_HUMIDITY_STATE))
-
-        if CORE_TEMPERATURE_STATE in states:
-            self.current_value = float(states.get(CORE_TEMPERATURE_STATE))
-
-        if CORE_ELECTRIC_POWER_CONSUMPTION_STATE in states:
-            self.current_value = float(
-                states.get(CORE_ELECTRIC_POWER_CONSUMPTION_STATE)
-            )
-
-        if CORE_ELECTRIC_ENERGY_CONSUMPTION_STATE in states:
-            self.current_value = (
-                float(states.get(CORE_ELECTRIC_ENERGY_CONSUMPTION_STATE)) / 1000
-            )
-
-        if CORE_CO_CONCENTRATION_STATE in states:
-            self.current_value = int(states.get(CORE_CO_CONCENTRATION_STATE))
-
-        if CORE_CO2_CONCENTRATION_STATE in states:
-            self.current_value = int(states.get(CORE_CO2_CONCENTRATION_STATE))
-
-        if CORE_WINDSPEED_STATE in states:
-            self.current_value = float(
-                "{:.2f}".format(states.get(CORE_WINDSPEED_STATE))
-            )
-
-        if CORE_SUN_ENERGY_STATE in states:
-            self.current_value = float(
-                "{:.2f}".format(states.get(CORE_SUN_ENERGY_STATE))
-            )
