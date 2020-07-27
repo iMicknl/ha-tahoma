fb16f84b2459fd0978e85b51caabe08f93232521
Mick Vleeshouwer <mick@imick.nl>
2020-07-27 22:41:15 +0200

    Squashed commit of the following:
    commit c7ca453a4521006a8074002949f55c896c17cf9a
    Author: Mick Vleeshouwer <mick@imick.nl>
    Date:   Mon Jul 27 16:32:04 2020 +0200
    
        Add device attributes to attributes (#180)
    
    commit 0883cf65f1217019e6f4b53d177d285064f42f01
    Author: tetienne <thibaut@etienne.pw>
    Date:   Mon Jul 27 14:14:38 2020 +0200
    
        Fix assumed state (#178)
    
    commit b490d79934f72a9019be7b6a33bfd312898017b2
    Author: tetienne <thibaut@etienne.pw>
    Date:   Mon Jul 27 14:02:58 2020 +0200
    
        Fix execute_command call (#176)
    
    commit 9560cfa0d8d0f9de2ca1ff150874a50e5be55d07
    Author: Mick Vleeshouwer <mick@imick.nl>
    Date:   Sun Jul 26 13:01:16 2020 +0200
    
        Add more ui_classes and sort them alphabetically (#168)
    
    commit b01f2ca7d0f26886a24445a63feb5475c1639a55
    Author: Mick Vleeshouwer <mick@imick.nl>
    Date:   Sun Jul 26 12:53:15 2020 +0200
    
        Improve support for GasSensor, ThermalEnergySensor, WaterSensor and add support for Siren Status (#170)

--- custom_components/tahoma/binary_sensor.py
+++ custom_components/tahoma/binary_sensor.py
@@ -17,6 +17,7 @@ from .tahoma_device import TahomaDevice
 
 SCAN_INTERVAL = timedelta(seconds=120)
 
+CORE_ASSEMBLY_STATE = "core:AssemblyState"
 CORE_BUTTON_STATE = "core:ButtonState"
 CORE_CONTACT_STATE = "core:ContactState"
 CORE_GAS_DETECTION_STATE = "core:GasDetectionState"
@@ -45,13 +46,15 @@ STATE_PRESSED = "pressed"
 TAHOMA_BINARY_SENSOR_DEVICE_CLASSES = {
     "AirFlowSensor": DEVICE_CLASS_GAS,
     "CarButtonSensor": DEVICE_CLASS_BUTTON,
-    "SmokeSensor": DEVICE_CLASS_SMOKE,
-    "OccupancySensor": DEVICE_CLASS_OCCUPANCY,
-    "MotionSensor": DEVICE_CLASS_MOTION,
     "ContactSensor": DEVICE_CLASS_OPENING,
-    "WindowHandle": DEVICE_CLASS_OPENING,
+    "MotionSensor": DEVICE_CLASS_MOTION,
+    "OccupancySensor": DEVICE_CLASS_OCCUPANCY,
     "RainSensor": DEVICE_CLASS_RAIN,
+    "SirenStatus": DEVICE_CLASS_OPENING,
+    "SmokeSensor": DEVICE_CLASS_SMOKE,
     "WaterDetectionSensor": DEVICE_CLASS_WATER,
+    "WaterSensor": DEVICE_CLASS_WATER,
+    "WindowHandle": DEVICE_CLASS_OPENING,
 }
 
 
@@ -75,6 +78,7 @@ class TahomaBinarySensor(TahomaDevice, BinarySensorEntity):
         """Return the state of the sensor."""
 
         return self.select_state(
+            CORE_ASSEMBLY_STATE,
             CORE_CONTACT_STATE,
             CORE_OCCUPANCY_STATE,
             CORE_SMOKE_STATE,
