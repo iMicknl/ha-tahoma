ea10e3d5fd1c84165801b640f444340a8b0bd948
Mick Vleeshouwer <mick@imick.nl>
2020-08-14 13:19:42 +0200

    Update custom_components/tahoma/alarm_control_panel.py
    Co-authored-by: Thibaut <thibaut@etienne.pw>

--- custom_components/tahoma/alarm_control_panel.py
+++ custom_components/tahoma/alarm_control_panel.py
@@ -167,7 +167,4 @@ class TahomaAlarmControlPanel(TahomaDevice, AlarmControlPanelEntity):
     @property
     def entity_registry_enabled_default(self) -> bool:
         """Return if the entity should be enabled when first added to the entity registry."""
-        if self.device.widget == "TSKAlarmController":
-            return False
-
-        return True
+       return self.device.widget != "TSKAlarmController"
