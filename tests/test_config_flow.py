19feef6dfc7942a2248fc35dcc8740c723eac291
Mick Vleeshouwer <mick@imick.nl>
2020-08-14 11:33:59 +0200

    Improve tests using constants and parametrize

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -2,16 +2,20 @@
 from asynctest import patch
 from custom_components.tahoma import config_flow
 from pyhoma.exceptions import BadCredentialsException, TooManyRequestsException
+import pytest
 
 from homeassistant import config_entries, setup
 
 from .common import MockConfigEntry
 
+TEST_EMAIL = "test@testdomain.com"
+TEST_PASSWORD = "test-password"
+
 
 async def test_form(hass):
     """Test we get the form."""
     await setup.async_setup_component(hass, "persistent_notification", {})
-    
+
     result = await hass.config_entries.flow.async_init(
         config_flow.DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
@@ -25,13 +29,15 @@ async def test_form(hass):
         "custom_components.tahoma.async_setup_entry", return_value=True
     ) as mock_setup_entry:
         result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {"username": "test@testdomain.com", "password": "test-password"},
+            result["flow_id"], {"username": TEST_EMAIL, "password": TEST_PASSWORD},
         )
 
     assert result2["type"] == "create_entry"
-    assert result2["title"] == "test@testdomain.com"
-    assert result2["data"] == {"username": "test@testdomain.com", "password": "test-password"}
+    assert result2["title"] == TEST_EMAIL
+    assert result2["data"] == {
+        "username": TEST_EMAIL,
+        "password": TEST_PASSWORD,
+    }
 
     await hass.async_block_till_done()
 
@@ -39,60 +45,35 @@ async def test_form(hass):
     assert len(mock_setup_entry.mock_calls) == 1
 
 
-async def test_form_invalid_auth(hass):
+@pytest.mark.parametrize(
+    "side_effect, error",
+    [
+        (BadCredentialsException, "invalid_auth",),
+        (TooManyRequestsException, "too_many_requests",),
+        (Exception, "unknown",),
+    ],
+)
+async def test_form_invalid(hass, side_effect, error):
     """Test we handle invalid auth."""
     result = await hass.config_entries.flow.async_init(
         config_flow.DOMAIN, context={"source": config_entries.SOURCE_USER}
     )
 
-    with patch("pyhoma.client.TahomaClient.login", side_effect=BadCredentialsException):
+    with patch("pyhoma.client.TahomaClient.login", side_effect=side_effect):
         result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {"username": "test@testdomain.com", "password": "test-password"},
+            result["flow_id"], {"username": TEST_EMAIL, "password": TEST_PASSWORD},
         )
 
     assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "invalid_auth"}
-
-
-async def test_form_too_many_requests(hass):
-    """Test we handle too many requests error."""
-    result = await hass.config_entries.flow.async_init(
-        config_flow.DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch(
-        "pyhoma.client.TahomaClient.login", side_effect=TooManyRequestsException
-    ):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {"username": "test@testdomain.com", "password": "test-password"},
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "too_many_requests"}
-
-
-async def test_form_cannot_connect(hass):
-    """Test we handle cannot connect error."""
-    result = await hass.config_entries.flow.async_init(
-        config_flow.DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
-
-    with patch("pyhoma.client.TahomaClient.login", side_effect=Exception):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {"username": "test@testdomain.com", "password": "test-password"},
-        )
-
-    assert result2["type"] == "form"
-    assert result2["errors"] == {"base": "unknown"}
+    assert result2["errors"] == {"base": error}
 
 
 async def test_abort_on_duplicate_entry(hass):
     """Test config flow aborts Config Flow on duplicate entries."""
     MockConfigEntry(
-        domain=config_flow.DOMAIN, unique_id="test@testdomain.com", data={"username": "test@testdomain.com", "password": "test-password"}
+        domain=config_flow.DOMAIN,
+        unique_id=TEST_EMAIL,
+        data={"username": TEST_EMAIL, "password": TEST_PASSWORD},
     ).add_to_hass(hass)
 
     result = await hass.config_entries.flow.async_init(
@@ -105,17 +86,19 @@ async def test_abort_on_duplicate_entry(hass):
         "custom_components.tahoma.async_setup_entry", return_value=True
     ) as mock_setup_entry:
         result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {"username": "test@testdomain.com", "password": "test-password"},
+            result["flow_id"], {"username": TEST_EMAIL, "password": TEST_PASSWORD},
         )
 
     assert result2["type"] == "abort"
     assert result2["reason"] == "already_configured"
 
+
 async def test_allow_multiple_unique_entries(hass):
     """Test config flow allows Config Flow unique entries."""
     MockConfigEntry(
-        domain=config_flow.DOMAIN, unique_id="test2@testdomain.com", data={"username": "test2@testdomain.com", "password": "test-password"}
+        domain=config_flow.DOMAIN,
+        unique_id="test2@testdomain.com",
+        data={"username": "test2@testdomain.com", "password": TEST_PASSWORD},
     ).add_to_hass(hass)
 
     result = await hass.config_entries.flow.async_init(
@@ -128,18 +111,19 @@ async def test_allow_multiple_unique_entries(hass):
         "custom_components.tahoma.async_setup_entry", return_value=True
     ) as mock_setup_entry:
         result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            {"username": "test@testdomain.com", "password": "test-password"},
+            result["flow_id"], {"username": TEST_EMAIL, "password": TEST_PASSWORD},
         )
 
     assert result2["type"] == "create_entry"
-    assert result2["title"] == "test@testdomain.com"
-    assert result2["data"] == {"username": "test@testdomain.com", "password": "test-password"}
+    assert result2["title"] == TEST_EMAIL
+    assert result2["data"] == {
+        "username": TEST_EMAIL,
+        "password": TEST_PASSWORD,
+    }
+
 
 async def test_import(hass):
     """Test config flow using configuration.yaml."""
-    await setup.async_setup_component(hass, "persistent_notification", {})
-
     with patch("pyhoma.client.TahomaClient.login", return_value=True), patch(
         "custom_components.tahoma.async_setup", return_value=True
     ) as mock_setup, patch(
@@ -148,13 +132,13 @@ async def test_import(hass):
         result = await hass.config_entries.flow.async_init(
             config_flow.DOMAIN,
             context={"source": config_entries.SOURCE_IMPORT},
-            data={"username": "test@testdomain.nl", "password": "test-password"},
+            data={"username": TEST_EMAIL, "password": TEST_PASSWORD},
         )
         assert result["type"] == "create_entry"
-        assert result["title"] == "test@testdomain.nl"
+        assert result["title"] == TEST_EMAIL
         assert result["data"] == {
-            "username": "test@testdomain.nl",
-            "password": "test-password",
+            "username": TEST_EMAIL,
+            "password": TEST_PASSWORD,
         }
 
         await hass.async_block_till_done()
