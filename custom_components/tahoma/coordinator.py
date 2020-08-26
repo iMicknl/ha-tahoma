2b897136ea5d6df12b6b35c7366db18375ca6353
Mick Vleeshouwer <mick@imick.nl>
2020-08-26 20:52:25 +0200

    Support retry on ServerDisconnectedError

--- custom_components/tahoma/coordinator.py
+++ custom_components/tahoma/coordinator.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 import logging
 from typing import Dict, List, Optional, Union
 
+from aiohttp import ServerDisconnectedError
 from pyhoma.client import TahomaClient
 from pyhoma.models import DataType, Device, State
 
@@ -43,20 +44,25 @@ class TahomaDataUpdateCoordinator(DataUpdateCoordinator):
         self.listener_id = listener_id
 
     async def _async_update_data(self) -> Dict[str, Device]:
-        """Fetch data from Tahoma."""
+        """Fetch TaHoma data via event listener."""
         try:
             events = await self.client.fetch_event_listener(self.listener_id)
+        except ServerDisconnectedError:
+            await self.client.login()
+            self.listener_id = await self.client.register_event_listener()
+            return self._async_update_data()
         except Exception as exception:
             raise UpdateFailed(f"Error communicating with the TaHoma API: {exception}")
-        else:
-            for event in events:
-                if event.name == "DeviceStateChangedEvent":
-                    for state in event.device_states:
-                        device = self.devices[event.deviceurl]
-                        if state.name not in device.states:
-                            device.states[state.name] = state
-                        device.states[state.name].value = self._get_state(state)
-            return self.devices
+
+        for event in events:
+            if event.name == "DeviceStateChangedEvent":
+                for state in event.device_states:
+                    device = self.devices[event.deviceurl]
+                    if state.name not in device.states:
+                        device.states[state.name] = state
+                    device.states[state.name].value = self._get_state(state)
+
+        return self.devices
 
     @staticmethod
     def _get_state(state: State) -> Union[float, int, bool, str, None]:
