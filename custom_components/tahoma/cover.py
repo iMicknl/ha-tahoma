f57f5388b02fe87b9c2347b0d230a9799afb3fcd
Mick Vleeshouwer <mick@imick.nl>
2020-08-26 20:31:21 +0200

    Fix typos

--- custom_components/tahoma/cover.py
+++ custom_components/tahoma/cover.py
@@ -105,7 +105,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
 
 class TahomaCover(TahomaDevice, CoverEntity):
-    """Representation a TaHoma Cover."""
+    """Representation of a TaHoma Cover."""
 
     @property
     def current_cover_position(self):
