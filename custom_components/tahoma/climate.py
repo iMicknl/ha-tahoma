49d3bae9c32ac62825c0f26059d8c9641cafe429
vlebourl <vlebourl@gmail.com>
2020-09-01 10:42:59 +0200

    Renamed to snake_case

--- custom_components/tahoma/climate.py
+++ custom_components/tahoma/climate.py
@@ -2,9 +2,9 @@
 
 from homeassistant.components.climate import DOMAIN as CLIMATE
 
-from .climate_devices.AtlanticElectricalHeater import AtlanticElectricalHeater
-from .climate_devices.DimmerExteriorHeating import DimmerExteriorHeating
-from .climate_devices.SomfyThermostat import SomfyThermostat
+from .climate_devices.atlantic_electrical_heater import AtlanticElectricalHeater
+from .climate_devices.dimmer_exterior_heating import DimmerExteriorHeating
+from .climate_devices.somfy_thermostat import SomfyThermostat
 from .const import DOMAIN
 
 TYPE = {
