d16443300efa831cc9dae5d0ad77721fb2a59687
Mick Vleeshouwer <mick@imick.nl>
2020-09-04 14:25:41 +0200

    Change logger
    Signed-off-by: Mick Vleeshouwer <mick@imick.nl>

--- custom_components/tahoma/cover.py
+++ custom_components/tahoma/cover.py
@@ -291,11 +291,11 @@ class TahomaCover(TahomaDevice, CoverEntity):
 
         # Cancelling a running execution will stop the cover movement
         if exec_id:
-            logging.debug("Cancelling command " + exec_id)
+            _LOGGER.debug("Cancelling command " + exec_id)
             await self.async_cancel_command(exec_id)
         # Fallback to available stop commands when executed started outside HA
         else:
-            logging.debug("Calling stop command")
+            _LOGGER.debug("Calling stop command")
             await self.async_execute_command(self.select_command(*stop_commands))
 
     async def async_my(self, **_):
