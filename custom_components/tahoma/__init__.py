d8af1a218784645e7f3e9ea95b5047fb45df52d1
Mick Vleeshouwer <mick@imick.nl>
2020-09-21 14:20:48 +0200

    Cancel task on unload entry
    Signed-off-by: Mick Vleeshouwer <mick@imick.nl>

--- custom_components/tahoma/__init__.py
+++ custom_components/tahoma/__init__.py
@@ -12,6 +12,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from aiohttp import CookieJar, ClientError, ServerDisconnectedError
 from pyhoma.client import TahomaClient
 from pyhoma.exceptions import (
     BadCredentialsException,
