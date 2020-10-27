1be9c7e069bf380536154ebb70d8c6a7171484ff
Mick Vleeshouwer <mick@imick.nl>
2020-10-27 09:43:17 +0100

    Improve handling of errors when Somfy server is in maintenance. (#306)
    Co-authored-by: Thibaut Etienne <thibaut@etienne.pw>

--- custom_components/tahoma/__init__.py
+++ custom_components/tahoma/__init__.py
@@ -12,7 +12,11 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import aiohttp_client, config_validation as cv
 from pyhoma.client import TahomaClient
-from pyhoma.exceptions import BadCredentialsException, TooManyRequestsException
+from pyhoma.exceptions import (
+    BadCredentialsException,
+    MaintenanceException,
+    TooManyRequestsException,
+)
 from pyhoma.models import Command
 import voluptuous as vol
 
@@ -94,6 +98,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         raise ConfigEntryNotReady from exception
     except (TimeoutError, ClientError, ServerDisconnectedError) as exception:
         _LOGGER.error("cannot_connect")
+        raise ConfigEntryNotReady from exception
+    except MaintenanceException as exception:
+        _LOGGER.error("server_in_maintenance")
         raise ConfigEntryNotReady from exception
     except Exception as exception:  # pylint: disable=broad-except
         _LOGGER.exception(exception)
