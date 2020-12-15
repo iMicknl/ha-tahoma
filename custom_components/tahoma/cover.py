53df48cd3ed44de92a7792895c21bf3a416eba41
Thibaut Etienne <thibaut@etienne.pw>
2020-12-15 14:11:23 +0100

    Add awning valence in more places

--- custom_components/tahoma/cover.py
+++ custom_components/tahoma/cover.py
@@ -157,7 +157,9 @@ class TahomaCover(TahomaDevice, CoverEntity):
         if position is None or position < 0 or position > 100:
             return None
 
-        if "Horizontal" not in self.device.widget:
+        if not (
+            "Horizontal" in self.device.widget or self.device.widget == "AwningValence"
+        ):
             position = 100 - position
 
         return position
@@ -178,7 +180,7 @@ class TahomaCover(TahomaDevice, CoverEntity):
         position = 100 - kwargs.get(ATTR_POSITION, 0)
 
         # HorizontalAwning devices need a reversed position that can not be obtained via the API
-        if "Horizontal" in self.device.widget:
+        if "Horizontal" in self.device.widget or self.device.widget == "AwningValance":
             position = kwargs.get(ATTR_POSITION, 0)
 
         await self.async_execute_command(
