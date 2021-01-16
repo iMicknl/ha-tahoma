iMicknl/ha-tahoma

commit c9aafec49828a62fbf9ca8f16a3b404cdc495a43

Clean up code for climate devices (#336)

custom_components/tahoma/climate_devices/atlantic_electrical_heater.py
@@ -30,13 +30,11 @@ MAP_PRESET_MODES = {
     PRESET_ECO: PRESET_ECO,
     PRESET_COMFORT: PRESET_COMFORT,
 }
-MAP_REVERSE_PRESET_MODES = {
-    PRESET_NONE: PRESET_OFF,
-    PRESET_FREEZE: PRESET_FROST_PROTECTION,
-    PRESET_ECO: PRESET_ECO,
-    PRESET_COMFORT: PRESET_COMFORT,
-}
+
+MAP_REVERSE_PRESET_MODES = {v: k for k, v in MAP_PRESET_MODES.items()}
+
 MAP_HVAC_MODES = {HVAC_MODE_HEAT: PRESET_COMFORT, HVAC_MODE_OFF: PRESET_OFF}
+MAP_REVERSE_HVAC_MODES = {v: k for k, v in MAP_HVAC_MODES.items()}
 
 
 class AtlanticElectricalHeater(TahomaDevice, ClimateEntity):
@@ -60,7 +58,7 @@ class AtlanticElectricalHeater(TahomaDevice, ClimateEntity):
     @property
     def hvac_modes(self) -> List[str]:
         """Return the list of available hvac operation modes."""
-        return [HVAC_MODE_OFF, HVAC_MODE_HEAT]
+        return [*MAP_REVERSE_HVAC_MODES]
 
     @property
     def preset_mode(self) -> Optional[str]:

custom_components/tahoma/climate_devices/dimmer_exterior_heating.py
@@ -10,6 +10,7 @@ from homeassistant.components.climate.const import (
 )
 from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
 
+from ..coordinator import TahomaDataUpdateCoordinator
 from ..tahoma_device import TahomaDevice
 
 _LOGGER = logging.getLogger(__name__)
@@ -23,9 +24,9 @@ CORE_LEVEL_STATE = "core:LevelState"
 class DimmerExteriorHeating(TahomaDevice, ClimateEntity):
     """Representation of TaHoma IO Atlantic Electrical Heater."""
 
-    def __init__(self, tahoma_device, controller):
+    def __init__(self, device_url: str, coordinator: TahomaDataUpdateCoordinator):
         """Init method."""
-        super().__init__(tahoma_device, controller)
+        super().__init__(device_url, coordinator)
         self._saved_level = 100 - self.select_state(CORE_LEVEL_STATE)
 
     @property

commit 518b3956e7e253e36a42649084053f14812e5cb7

fix bug in hvac modes introduced in #336

custom_components/tahoma/climate_devices/atlantic_electrical_heater.py
@@ -58,7 +58,7 @@ class AtlanticElectricalHeater(TahomaDevice, ClimateEntity):
     @property
     def hvac_modes(self) -> List[str]:
         """Return the list of available hvac operation modes."""
-        return [*MAP_REVERSE_HVAC_MODES]
+        return [*MAP_HVAC_MODES]
 
     @property
     def preset_mode(self) -> Optional[str]: