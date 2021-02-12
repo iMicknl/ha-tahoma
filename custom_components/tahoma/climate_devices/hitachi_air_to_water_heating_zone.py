4af1932fa767b4d43ec2d3abcc460b8a5b5c327e
Mick Vleeshouwer <mick@imick.nl>
2021-02-12 18:25:13 +0100

    Add presets

--- custom_components/tahoma/climate_devices/hitachi_air_to_water_heating_zone.py
+++ custom_components/tahoma/climate_devices/hitachi_air_to_water_heating_zone.py
@@ -1,6 +1,6 @@
 """Support for HitachiAirToWaterHeatingZone."""
 import logging
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import (
@@ -10,6 +10,9 @@ from homeassistant.components.climate.const import (
     HVAC_MODE_FAN_ONLY,
     HVAC_MODE_HEAT,
     HVAC_MODE_OFF,
+    PRESET_COMFORT,
+    PRESET_ECO,
+    SUPPORT_PRESET_MODE,
 )
 from homeassistant.const import TEMP_CELSIUS
 
@@ -17,8 +20,14 @@ from ..tahoma_device import TahomaDevice
 
 _LOGGER = logging.getLogger(__name__)
 
+COMMAND_SET_TARGET_MODE = "setTargetMode"
 
-TAHOMA_TO_HVAC_MODES = {
+MODBUS_YUTAKI_TARGET_MODE_STATE = "modbus:YutakiTargetModeState"
+
+PRESET_STATE_ECO = "eco"
+PRESET_STATE_COMFORT = "comfort"
+
+TAHOMA_TO_HVAC_MODE = {
     "heating": HVAC_MODE_HEAT,
     "off": HVAC_MODE_OFF,
     "fan": HVAC_MODE_FAN_ONLY,
@@ -27,11 +36,26 @@ TAHOMA_TO_HVAC_MODES = {
     "auto": HVAC_MODE_AUTO,
 }
 
-HVAC_MODES_TO_TAHOMA = {v: k for k, v in TAHOMA_TO_HVAC_MODES.items()}
+HVAC_MODE_TO_TAHOMA = {v: k for k, v in TAHOMA_TO_HVAC_MODE.items()}
+
+TAHOMA_TO_PRESET_MODE = {
+    PRESET_STATE_COMFORT: PRESET_COMFORT,
+    PRESET_STATE_ECO: PRESET_ECO,
+}
+
+PRESET_MODE_TO_TAHOMA = {v: k for k, v in TAHOMA_TO_PRESET_MODE.items()}
 
 
 class HitachiAirToWaterHeatingZone(TahomaDevice, ClimateEntity):
     """Representation of HitachiAirToWaterHeatingZone."""
+
+    @property
+    def device_info(self) -> Dict[str, Any]:
+        """Return the device state attributes."""
+        device_info = super().device_info or {}
+        device_info["manufacturer"] = "Hitachi"
+
+        return device_info
 
     @property
     def temperature_unit(self) -> str:
@@ -41,7 +65,7 @@ class HitachiAirToWaterHeatingZone(TahomaDevice, ClimateEntity):
     @property
     def supported_features(self) -> int:
         """Return the list of supported features."""
-        return 0
+        return SUPPORT_PRESET_MODE
 
     @property
     def hvac_mode(self) -> str:
@@ -58,9 +82,17 @@ class HitachiAirToWaterHeatingZone(TahomaDevice, ClimateEntity):
         # TODO implement
 
     @property
-    def device_info(self) -> Dict[str, Any]:
-        """Return the device state attributes."""
-        device_info = super().device_info or {}
-        device_info["manufacturer"] = "Hitachi"
+    def preset_modes(self) -> Optional[List[str]]:
+        """Return a list of available preset modes."""
+        return [*PRESET_MODE_TO_TAHOMA]
 
-        return device_info
+    @property
+    def preset_mode(self) -> Optional[str]:
+        """Return the current preset mode, e.g., home, away, temp."""
+        return TAHOMA_TO_PRESET_MODE[self.select_state(MODBUS_YUTAKI_TARGET_MODE_STATE)]
+
+    async def async_set_preset_mode(self, preset_mode: str) -> None:
+        """Set new preset mode."""
+        await self.async_execute_command(
+            COMMAND_SET_TARGET_MODE, TAHOMA_TO_PRESET_MODE[preset_mode]
+        )
