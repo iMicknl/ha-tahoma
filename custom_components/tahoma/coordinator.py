1fa882a0f4ffc649f2c3b7df648d27ae19129da0
Vincent Le Bourlot <vlebourl@gmail.com>
2021-02-16 11:33:43 +0100

    fix typing

--- custom_components/tahoma/coordinator.py
+++ custom_components/tahoma/coordinator.py
@@ -146,7 +146,7 @@ class TahomaDataUpdateCoordinator(DataUpdateCoordinator):
     @staticmethod
     def _get_state(
         state: State,
-    ) -> Union[Dict[Any], List[Any], float, int, bool, str, None]:
+    ) -> Union[Dict[Any, Any], List[Any], float, int, bool, str, None]:
         """Cast string value to the right type."""
         if state.type in [DataType.JSON_ARRAY, DataType.JSON_OBJECT]:
             return json.loads(state.value)
