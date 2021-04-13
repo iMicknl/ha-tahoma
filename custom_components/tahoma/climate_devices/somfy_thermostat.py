093504f0930867c7436a991a21843c0b30c67421
Vincent Le Bourlot <vlebourl@gmail.com>
2021-04-13 09:40:19 +0200

    fix style

--- custom_components/tahoma/climate_devices/somfy_thermostat.py
+++ custom_components/tahoma/climate_devices/somfy_thermostat.py
@@ -142,7 +142,11 @@ class SomfyThermostat(TahomaEntity, ClimateEntity):
     @callback
     def update_temp(self, state):
         """Update thermostat with latest state from sensor."""
-        if state is None or state.state == STATE_UNKNOWN or state.state == STATE_UNAVAILABLE:
+        if (
+            state is None
+            or state.state == STATE_UNKNOWN
+            or state.state == STATE_UNAVAILABLE
+        ):
             return
 
         try:
