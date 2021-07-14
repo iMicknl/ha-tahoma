a2119d5134fd0f15356031a80e07383b3731a0ca
Mick Vleeshouwer <mick@imick.nl>
2021-07-14 23:27:44 +0200

    Update presets and other features

--- custom_components/tahoma/climate_devices/atlantic_electrical_towel_dryer.py
+++ custom_components/tahoma/climate_devices/atlantic_electrical_towel_dryer.py
@@ -1,43 +1,66 @@
-"""Support for Atlantic Electrical Heater IO controller."""
+"""Support for Atlantic Electrical Towel Dryer."""
+import logging
 from typing import List, Optional
 
 from homeassistant.components.climate import (
-    HVAC_MODE_OFF,
     SUPPORT_PRESET_MODE,
+    SUPPORT_TARGET_TEMPERATURE,
     ClimateEntity,
 )
 from homeassistant.components.climate.const import (
+    HVAC_MODE_AUTO,
     HVAC_MODE_HEAT,
-    PRESET_COMFORT,
-    PRESET_ECO,
+    HVAC_MODE_OFF,
     PRESET_NONE,
 )
-from homeassistant.const import TEMP_CELSIUS
+from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
 
 from ..tahoma_entity import TahomaEntity
 
-COMMAND_SET_HEATING_LEVEL = "setHeatingLevel"
+_LOGGER = logging.getLogger(__name__)
 
+COMMAND_SET_HEATING_LEVEL = "setHeatingLevel"
+COMMAND_SET_TARGET_TEMPERATURE = "setTargetTemperature"
+COMMAND_SET_OPERATING_MODE = "setOperatingMode"
+
+CORE_OPERATING_MODE_STATE = "core:OperatingModeState"
+CORE_TARGET_TEMPERATURE_STATE = "core:TargetTemperatureState"
+CORE_ON_OFF_STATE = "core:OnOffState"
 IO_TARGET_HEATING_LEVEL_STATE = "io:TargetHeatingLevelState"
 
-PRESET_FREEZE = "Freeze"
-PRESET_FROST_PROTECTION = "frostprotection"
-PRESET_OFF = "off"
+PRESET_BOOST = "boost"
+PRESET_DRYING = "drying"
+PRESET_FROST_PROTECTION = "frost_protection"
 
-MAP_PRESET_MODES = {
-    PRESET_OFF: PRESET_NONE,
-    PRESET_FROST_PROTECTION: PRESET_FREEZE,
-    PRESET_ECO: PRESET_ECO,
-    PRESET_COMFORT: PRESET_COMFORT,
+PRESET_STATE_FROST_PROTECTION = "frostprotection"
+PRESET_STATE_OFF = "off"
+PRESET_STATE_ECO = "eco"
+PRESET_STATE_BOOST = "boost"
+PRESET_STATE_COMFORT = "comfort"
+PRESET_STATE_COMFORT1 = "comfort-1"
+PRESET_STATE_COMFORT2 = "comfort-2"
+
+# Map Home Assistant presets to TaHoma presets
+PRESET_MODE_TO_TAHOMA = {
+    PRESET_BOOST: "boost",
+    PRESET_DRYING: "drying",
+    PRESET_NONE: "permanentHeating",
 }
 
-MAP_REVERSE_PRESET_MODES = {v: k for k, v in MAP_PRESET_MODES.items()}
+TAHOMA_TO_PRESET_MODE = {v: k for k, v in PRESET_MODE_TO_TAHOMA.items()}
 
-MAP_HVAC_MODES = {HVAC_MODE_HEAT: PRESET_COMFORT, HVAC_MODE_OFF: PRESET_OFF}
+# Map TaHoma HVAC modes to Home Assistant HVAC modes
+TAHOMA_TO_HVAC_MODE = {
+    "external": HVAC_MODE_HEAT,  # manu
+    "standby": HVAC_MODE_OFF,
+    "internal": HVAC_MODE_AUTO,  # prog
+}
+
+HVAC_MODE_TO_TAHOMA = {v: k for k, v in TAHOMA_TO_HVAC_MODE.items()}
 
 
 class AtlanticElectricalTowelDryer(TahomaEntity, ClimateEntity):
-    """Representation of TaHoma IO Atlantic Electrical Heater."""
+    """Representation of Atlantic Electrical Towel Dryer."""
 
     @property
     def temperature_unit(self) -> str:
@@ -47,40 +70,86 @@ class AtlanticElectricalTowelDryer(TahomaEntity, ClimateEntity):
     @property
     def supported_features(self) -> int:
         """Return the list of supported features."""
-        return SUPPORT_PRESET_MODE
+        supported_features = 0
 
-    @property
-    def hvac_mode(self) -> str:
-        """Return hvac operation ie. heat, cool mode."""
-        return HVAC_MODE_OFF if self.preset_mode == PRESET_NONE else HVAC_MODE_HEAT
+        supported_features |= SUPPORT_PRESET_MODE
+        supported_features |= SUPPORT_TARGET_TEMPERATURE
+
+        return supported_features
 
     @property
     def hvac_modes(self) -> List[str]:
         """Return the list of available hvac operation modes."""
-        return [*MAP_HVAC_MODES]
+        return [*HVAC_MODE_TO_TAHOMA]
 
     @property
-    def preset_mode(self) -> Optional[str]:
-        """Return the current preset mode, e.g., home, away, temp."""
-        return MAP_PRESET_MODES[self.select_state(IO_TARGET_HEATING_LEVEL_STATE)]
-
-    @property
-    def preset_modes(self) -> Optional[List[str]]:
-        """Return a list of available preset modes."""
-        return [PRESET_NONE, PRESET_FREEZE, PRESET_ECO, PRESET_COMFORT]
+    def hvac_mode(self) -> str:
+        """Return hvac operation ie. heat, cool mode."""
+        if CORE_OPERATING_MODE_STATE in self.device.states:
+            return TAHOMA_TO_HVAC_MODE[self.select_state(CORE_OPERATING_MODE_STATE)]
+        if CORE_ON_OFF_STATE in self.device.states:
+            return TAHOMA_TO_HVAC_MODE[self.select_state(CORE_ON_OFF_STATE)]
 
     async def async_set_hvac_mode(self, hvac_mode: str) -> None:
         """Set new target hvac mode."""
         await self.async_execute_command(
-            COMMAND_SET_HEATING_LEVEL, MAP_HVAC_MODES[hvac_mode]
+            "setTowelDryerOperatingMode", HVAC_MODE_TO_TAHOMA[hvac_mode]
         )
+
+    @property
+    def preset_modes(self) -> Optional[List[str]]:
+        """Return a list of available preset modes."""
+        return [*PRESET_MODE_TO_TAHOMA]
+
+    @property
+    def preset_mode(self) -> Optional[str]:
+        """Return the current preset mode, e.g., home, away, temp."""
+        # io:TowelDryerTemporaryStateState
+        return TAHOMA_TO_PRESET_MODE[
+            self.select_state("io:TowelDryerTemporaryStateState")
+        ]
 
     async def async_set_preset_mode(self, preset_mode: str) -> None:
         """Set new preset mode."""
         await self.async_execute_command(
-            COMMAND_SET_HEATING_LEVEL, MAP_REVERSE_PRESET_MODES[preset_mode]
+            "setTowelDryerTemporaryState", PRESET_MODE_TO_TAHOMA[preset_mode]
         )
 
-    async def async_turn_off(self) -> None:
-        """Turn off the device."""
-        await self.async_execute_command(COMMAND_SET_HEATING_LEVEL, PRESET_OFF)
+    # async def async_turn_on(self) -> None:
+    #     """Turn on the device."""
+    #     await self.async_execute_command(COMMAND_SET_OPERATING_MODE, "on")
+
+    # async def async_turn_off(self) -> None:
+    #     """Turn off the device."""
+    #     await self.async_execute_command(COMMAND_SET_OPERATING_MODE, "off")
+
+    @property
+    def target_temperature(self) -> None:
+        """Return the temperature."""
+
+        # return self.select_state(CORE_TARGET_TEMPERATURE_STATE)
+
+        if self.hvac_mode == HVAC_MODE_AUTO:
+            return self.select_state("io:EffectiveTemperatureSetpointState")
+        else:
+            return self.select_state(CORE_TARGET_TEMPERATURE_STATE)
+
+    @property
+    def current_temperature(self):
+        """Return current temperature."""
+        return self.select_state("core:ComfortRoomTemperatureState")
+
+    async def async_set_temperature(self, **kwargs) -> None:
+        """Set new temperature."""
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+
+        await self.async_execute_command(COMMAND_SET_TARGET_TEMPERATURE, temperature)
+
+        # if self.hvac_mode == HVAC_MODE_AUTO:
+        #     await self.async_execute_command(
+        #         "setDerogatedTargetTemperature", temperature
+        #     )
+        # else:
+        #     await self.async_execute_command(
+        #         COMMAND_SET_TARGET_TEMPERATURE, temperature
+        #     )
