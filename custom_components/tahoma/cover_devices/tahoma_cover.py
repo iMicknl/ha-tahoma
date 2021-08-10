2b8a4ca1ca1e1e2c8eddd315e28ee680e3909d2b
Mick Vleeshouwer <mick@imick.nl>
2021-08-10 09:46:16 +0200

    Rollback icon logic in cover

--- custom_components/tahoma/cover_devices/tahoma_cover.py
+++ custom_components/tahoma/cover_devices/tahoma_cover.py
@@ -62,6 +62,10 @@ CORE_TARGET_CLOSURE_STATE = "core:TargetClosureState"
 MYFOX_SHUTTER_STATUS_STATE = "myfox:ShutterStatusState"
 
 IO_PRIORITY_LOCK_LEVEL_STATE = "io:PriorityLockLevelState"
+IO_PRIORITY_LOCK_ORIGINATOR_STATE = "io:PriorityLockOriginatorState"
+
+ICON_LOCK_ALERT = "mdi:lock-alert"
+ICON_WEATHER_WINDY = "mdi:weather-windy"
 
 STATE_CLOSED = "closed"
 
@@ -275,3 +279,16 @@ class TahomaGenericCover(TahomaEntity, CoverEntity):
             supported_features |= SUPPORT_MY
 
         return supported_features
+
+    @property
+    def icon(self):
+        """Return the icon to use in the frontend, if any."""
+        if (
+            self.has_state(CORE_PRIORITY_LOCK_TIMER_STATE)
+            and self.select_state(CORE_PRIORITY_LOCK_TIMER_STATE) > 0
+        ):
+            if self.select_state(IO_PRIORITY_LOCK_ORIGINATOR_STATE) == "wind":
+                return ICON_WEATHER_WINDY
+            return ICON_LOCK_ALERT
+
+        return None
