607b72b1c078ceaae44dc49fc8153a8ca95e0209
Thibaut <thibaut@etienne.pw>
2021-10-03 18:18:34 +0200

    Remove polling custom interval and reduce it for stateless configuration (#561)

--- custom_components/tahoma/coordinator.py
+++ custom_components/tahoma/coordinator.py
@@ -19,7 +19,7 @@ from pyhoma.exceptions import (
 )
 from pyhoma.models import DataType, Device, Place, State
 
-from .const import DOMAIN
+from .const import DOMAIN, UPDATE_INTERVAL
 
 TYPES = {
     DataType.NONE: None,
@@ -59,9 +59,13 @@ class OverkizDataUpdateCoordinator(DataUpdateCoordinator):
         )
 
         self.data = {}
-        self.original_update_interval = update_interval
         self.client = client
         self.devices: Dict[str, Device] = {d.deviceurl: d for d in devices}
+        self.is_stateless = all(
+            device.deviceurl.startswith("rts://")
+            or device.deviceurl.startswith("internal://")
+            for device in devices
+        )
         self.executions: Dict[str, Dict[str, str]] = {}
         self.areas = self.places_to_area(places)
         self._config_entry_id = config_entry_id
@@ -146,7 +150,8 @@ class OverkizDataUpdateCoordinator(DataUpdateCoordinator):
                 if event.exec_id not in self.executions:
                     self.executions[event.exec_id] = {}
 
-                self.update_interval = timedelta(seconds=1)
+                if not self.is_stateless:
+                    self.update_interval = timedelta(seconds=1)
 
             elif (
                 event.name == EventName.EXECUTION_STATE_CHANGED
@@ -156,7 +161,7 @@ class OverkizDataUpdateCoordinator(DataUpdateCoordinator):
                 del self.executions[event.exec_id]
 
         if not self.executions:
-            self.update_interval = self.original_update_interval
+            self.update_interval = UPDATE_INTERVAL
 
         return self.devices
 
