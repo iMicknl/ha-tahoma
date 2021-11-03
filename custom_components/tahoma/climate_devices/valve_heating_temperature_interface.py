ee5be27bd94b4e49ec780c42f7076b5854c3bd5e
Mick Vleeshouwer <mick@imick.nl>
2021-11-03 19:59:12 +0100

    Fix

--- custom_components/tahoma/climate_devices/valve_heating_temperature_interface.py
+++ custom_components/tahoma/climate_devices/valve_heating_temperature_interface.py
@@ -4,6 +4,7 @@ import logging
 from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import (
     HVAC_MODE_HEAT,
+    SUPPORT_PRESET_MODE,
     SUPPORT_TARGET_TEMPERATURE,
 )
 from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
@@ -12,19 +13,32 @@ from ..entity import OverkizEntity
 
 _LOGGER = logging.getLogger(__name__)
 
+# Map Overkiz HVAC modes to Home Assistant HVAC modes
+OVERKIZ_TO_HVAC_MODE = {
+    None: HVAC_MODE_HEAT,
+}
+HVAC_MODE_TO_OVERKIZ = {v: k for k, v in OVERKIZ_TO_HVAC_MODE.items()}
+
+# Map Home Assistant presets to TaHoma presets
+PRESET_MODE_TO_OVERKIZ = {
+    "comfort": "comfort",
+    "eco": "eco",
+    "away": "away",
+    "frostprotection": "frostprotection",
+    "geofencing_mode": "geofencingMode",
+}
+
+OVERKIZ_TO_PRESET_MODE = {v: k for k, v in PRESET_MODE_TO_OVERKIZ.items()}
+
 
 class ValveHeatingTemperatureInterface(OverkizEntity, ClimateEntity):
     """Representation of Valve Heating Temperature Interface device."""
 
-    _attr_hvac_mode = None
+    _attr_hvac_mode = HVAC_MODE_HEAT
     _attr_hvac_modes = [HVAC_MODE_HEAT]
-    _attr_preset_mode = None
-    _attr_supported_features = SUPPORT_TARGET_TEMPERATURE
+    _attr_preset_modes = [*PRESET_MODE_TO_OVERKIZ]
+    _attr_supported_features = SUPPORT_PRESET_MODE | SUPPORT_TARGET_TEMPERATURE
     _attr_temperature_unit = TEMP_CELSIUS
-
-    async def async_set_hvac_mode(self, hvac_mode: str) -> None:
-        """Set new target hvac mode."""
-        return
 
     @property
     def target_temperature(self) -> None:
@@ -40,3 +54,23 @@ class ValveHeatingTemperatureInterface(OverkizEntity, ClimateEntity):
         """Set new temperature."""
         temperature = kwargs.get(ATTR_TEMPERATURE)
         await self.executor.async_execute_command("", temperature)
+
+    async def async_set_hvac_mode(self, hvac_mode: str) -> None:
+        """Set new target hvac mode."""
+
+        # TODO
+
+        return
+
+    @property
+    def preset_mode(self) -> str:
+        """Return the current preset mode, e.g., home, away, temp."""
+        return OVERKIZ_TO_PRESET_MODE[
+            self.executor.select_state("io:DerogationHeatingModeState")
+        ]
+
+    async def async_set_preset_mode(self, preset_mode: str) -> None:
+        """Set new preset mode."""
+        await self.executor.async_execute_command(
+            "setDerogation", PRESET_MODE_TO_OVERKIZ[preset_mode]
+        )
