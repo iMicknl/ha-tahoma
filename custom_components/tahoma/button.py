31ba162fe4307ed8179458f0379db29bb7c6bef3
Mick Vleeshouwer <mick@imick.nl>
2022-01-13 21:44:32 +0100

    Backport button

--- custom_components/tahoma/button.py
+++ custom_components/tahoma/button.py
@@ -1,14 +1,17 @@
-"""Support for Overkiz number devices."""
+"""Support for Overkiz (virtual) buttons."""
+from __future__ import annotations
+
 from homeassistant.components.button import ButtonEntity, ButtonEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ENTITY_CATEGORY_DIAGNOSTIC
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
+from . import HomeAssistantOverkizData
 from .const import DOMAIN, IGNORED_OVERKIZ_DEVICES
 from .entity import OverkizDescriptiveEntity
 
-BUTTON_DESCRIPTIONS = [
+BUTTON_DESCRIPTIONS: list[ButtonEntityDescription] = [
     # My Position (cover, light)
     ButtonEntityDescription(
         key="my",
@@ -17,7 +20,7 @@ BUTTON_DESCRIPTIONS = [
     ),
     # Identify
     ButtonEntityDescription(
-        key="identify",  # startIdentify and identify are reversed... Remove when fixed server side.
+        key="identify",  # startIdentify and identify are reversed... Swap this when fixed in API.
         name="Start Identify",
         icon="mdi:human-greeting-variant",
         entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
@@ -31,11 +34,18 @@ BUTTON_DESCRIPTIONS = [
         entity_registry_enabled_default=False,
     ),
     ButtonEntityDescription(
-        key="startIdentify",  # startIdentify and identify are reversed... Remove when fixed server side.
+        key="startIdentify",  # startIdentify and identify are reversed... Swap this when fixed in API.
         name="Identify",
         icon="mdi:human-greeting-variant",
         entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
     ),
+    # RTDIndoorSiren / RTDOutdoorSiren
+    ButtonEntityDescription(key="dingDong", name="Ding Dong", icon="mdi:bell-ring"),
+    ButtonEntityDescription(key="bip", name="Bip", icon="mdi:bell-ring"),
+    ButtonEntityDescription(
+        key="fastBipSequence", name="Fast Bip Sequence", icon="mdi:bell-ring"
+    ),
+    ButtonEntityDescription(key="ring", name="Ring", icon="mdi:bell-ring"),
 ]
 
 
@@ -43,37 +53,37 @@ async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
-):
-    """Set up the Overkiz number from a config entry."""
-    data = hass.data[DOMAIN][entry.entry_id]
-    coordinator = data["coordinator"]
-
-    entities = []
+) -> None:
+    """Set up the Overkiz button from a config entry."""
+    data: HomeAssistantOverkizData = hass.data[DOMAIN][entry.entry_id]
+    entities: list[ButtonEntity] = []
 
     supported_commands = {
         description.key: description for description in BUTTON_DESCRIPTIONS
     }
 
-    for device in coordinator.data.values():
+    for device in data.coordinator.data.values():
         if (
-            device.widget not in IGNORED_OVERKIZ_DEVICES
-            and device.ui_class not in IGNORED_OVERKIZ_DEVICES
+            device.widget in IGNORED_OVERKIZ_DEVICES
+            or device.ui_class in IGNORED_OVERKIZ_DEVICES
         ):
-            for command in device.definition.commands:
-                if description := supported_commands.get(command.command_name):
-                    entities.append(
-                        OverkizButton(
-                            device.device_url,
-                            coordinator,
-                            description,
-                        )
+            continue
+
+        for command in device.definition.commands:
+            if description := supported_commands.get(command.command_name):
+                entities.append(
+                    OverkizButton(
+                        device.device_url,
+                        data.coordinator,
+                        description,
                     )
+                )
 
     async_add_entities(entities)
 
 
 class OverkizButton(OverkizDescriptiveEntity, ButtonEntity):
-    """Representation of an Overkiz Button entity."""
+    """Representation of an Overkiz Button."""
 
     async def async_press(self) -> None:
         """Handle the button press."""
