62d8dffaf27577d402f193ff3b1682c0627e7562
Mick Vleeshouwer <mick@imick.nl>
2022-01-16 12:01:37 +0100

    Rewrite coordinator

--- custom_components/tahoma/coordinator.py
+++ custom_components/tahoma/coordinator.py
@@ -67,9 +67,7 @@ class OverkizDataUpdateCoordinator(DataUpdateCoordinator[Dict[str, Device]]):
         try:
             events = await self.client.fetch_events()
         except BadCredentialsException as exception:
-            # Keep retrying until Somfy fixes their servers (https://github.com/iMicknl/ha-tahoma/issues/599)
             raise UpdateFailed("Invalid authentication.") from exception
-            # raise ConfigEntryAuthFailed() from exception
         except TooManyRequestsException as exception:
             raise UpdateFailed("Too many requests, try again later.") from exception
         except MaintenanceException as exception:
@@ -84,9 +82,7 @@ class OverkizDataUpdateCoordinator(DataUpdateCoordinator[Dict[str, Device]]):
                 await self.client.login()
                 self.devices = await self._get_devices()
             except BadCredentialsException as exception:
-                # Keep retrying until Somfy fixes their servers (https://github.com/iMicknl/ha-tahoma/issues/599)
                 raise UpdateFailed("Invalid authentication.") from exception
-                # raise ConfigEntryAuthFailed() from exception
             except TooManyRequestsException as exception:
                 raise UpdateFailed("Too many requests, try again later.") from exception
 
