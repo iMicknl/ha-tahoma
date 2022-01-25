8a2c4cb1c5a00729843c5959ff0fd5386059a965
Mick Vleeshouwer <mick@imick.nl>
2022-01-26 00:20:12 +0100

    Update const

--- custom_components/tahoma/const.py
+++ custom_components/tahoma/const.py
@@ -19,11 +19,16 @@ UPDATE_INTERVAL_ALL_ASSUMED_STATE: Final = timedelta(minutes=60)
 PLATFORMS: list[Platform] = [
     Platform.BINARY_SENSOR,
     Platform.BUTTON,
+    Platform.CLIMATE,
+    Platform.COVER,
     Platform.LIGHT,
     Platform.LOCK,
     Platform.NUMBER,
     Platform.SCENE,
     Platform.SENSOR,
+    Platform.SIREN,
+    Platform.SWITCH,
+    Platform.WATER_HEATER,
 ]
 
 IGNORED_OVERKIZ_DEVICES: list[UIClass | UIWidget] = [
@@ -32,46 +37,48 @@ IGNORED_OVERKIZ_DEVICES: list[UIClass | UIWidget] = [
 ]
 
 # Used to map the Somfy widget and ui_class to the Home Assistant platform
-OVERKIZ_DEVICE_TO_PLATFORM: dict[UIClass | UIWidget, Platform] = {
+OVERKIZ_DEVICE_TO_PLATFORM: dict[UIClass | UIWidget, Platform | None] = {
     UIClass.ADJUSTABLE_SLATS_ROLLER_SHUTTER: Platform.COVER,
     UIClass.ALARM: Platform.ALARM_CONTROL_PANEL,
+    UIClass.AWNING: Platform.COVER,
+    UIClass.CURTAIN: Platform.COVER,
+    UIClass.DOOR_LOCK: Platform.LOCK,
+    UIClass.EXTERIOR_SCREEN: Platform.COVER,
+    UIClass.EXTERIOR_VENETIAN_BLIND: Platform.COVER,
+    UIClass.GARAGE_DOOR: Platform.COVER,
+    UIClass.GATE: Platform.COVER,
+    UIClass.LIGHT: Platform.LIGHT,
+    UIClass.ON_OFF: Platform.SWITCH,
+    UIClass.PERGOLA: Platform.COVER,
+    UIClass.ROLLER_SHUTTER: Platform.COVER,
+    UIClass.SCREEN: Platform.COVER,
+    UIClass.SHUTTER: Platform.COVER,
+    UIClass.SIREN: Platform.SIREN,
+    UIClass.SWIMMING_POOL: Platform.SWITCH,
+    UIClass.SWINGING_SHUTTER: Platform.COVER,
+    UIClass.VENETIAN_BLIND: Platform.COVER,
+    UIClass.WINDOW: Platform.COVER,
     UIWidget.ATLANTIC_ELECTRICAL_HEATER: Platform.CLIMATE,  # widgetName, uiClass is HeatingSystem (not supported)
     UIWidget.ATLANTIC_ELECTRICAL_HEATER_WITH_ADJUSTABLE_TEMPERATURE_SETPOINT: Platform.CLIMATE,  # widgetName, uiClass is HeatingSystem (not supported)
     UIWidget.ATLANTIC_ELECTRICAL_TOWEL_DRYER: Platform.CLIMATE,  # widgetName, uiClass is HeatingSystem (not supported)
     UIWidget.ATLANTIC_PASS_APC_DHW: Platform.CLIMATE,  # widgetName, uiClass is WaterHeatingSystem (not supported)
     UIWidget.ATLANTIC_PASS_APC_HEATING_AND_COOLING_ZONE: Platform.CLIMATE,  # widgetName, uiClass is HeatingSystem (not supported)
     UIWidget.ATLANTIC_PASS_APC_ZONE_CONTROL: Platform.CLIMATE,  # widgetName, uiClass is HeatingSystem (not supported)
-    UIClass.AWNING: Platform.COVER,
-    UIClass.CURTAIN: Platform.COVER,
     UIWidget.DIMMER_EXTERIOR_HEATING: Platform.CLIMATE,  # widgetName, uiClass is ExteriorHeatingSystem (not supported)
     UIWidget.DOMESTIC_HOT_WATER_PRODUCTION: Platform.WATER_HEATER,  # widgetName, uiClass is WaterHeatingSystem (not supported)
     UIWidget.DOMESTIC_HOT_WATER_TANK: Platform.SWITCH,  # widgetName, uiClass is WaterHeatingSystem (not supported)
-    UIClass.DOOR_LOCK: Platform.LOCK,
     UIWidget.EVO_HOME_CONTROLLER: Platform.CLIMATE,  # widgetName, uiClass is EvoHome (not supported)
-    UIClass.EXTERIOR_SCREEN: Platform.COVER,
-    UIClass.EXTERIOR_VENETIAN_BLIND: Platform.COVER,
-    UIClass.GARAGE_DOOR: Platform.COVER,
-    UIClass.GATE: Platform.COVER,
     UIWidget.HEATING_SET_POINT: Platform.CLIMATE,  # widgetName, uiClass is EvoHome (not supported)
-    UIWidget.HITACHI_DHW: Platform.WATER_HEATER,  # widgetName, uiClass is HitachiHeatingSystem (not supported)
-    UIWidget.HITACHI_AIR_TO_WATER_HEATING_ZONE: Platform.CLIMATE,  # widgetName, uiClass is HitachiHeatingSystem (not supported)
     UIWidget.HITACHI_AIR_TO_AIR_HEAT_PUMP: Platform.CLIMATE,  # widgetName, uiClass is HitachiHeatingSystem (not supported)
-    UIClass.LIGHT: Platform.LIGHT,
+    UIWidget.HITACHI_AIR_TO_WATER_HEATING_ZONE: Platform.CLIMATE,  # widgetName, uiClass is HitachiHeatingSystem (not supported)
+    UIWidget.HITACHI_DHW: Platform.WATER_HEATER,  # widgetName, uiClass is HitachiHeatingSystem (not supported)
     UIWidget.MY_FOX_SECURITY_CAMERA: Platform.COVER,  # widgetName, uiClass is Camera (not supported)
-    UIClass.ON_OFF: Platform.SWITCH,
-    UIClass.PERGOLA: Platform.COVER,
-    UIClass.ROLLER_SHUTTER: Platform.COVER,
+    UIWidget.RTD_INDOOR_SIREN: Platform.SWITCH,  # widgetName, uiClass is Siren (siren)
+    UIWidget.RTD_OUTDOOR_SIREN: Platform.SWITCH,  # widgetName, uiClass is Siren (siren)
     UIWidget.RTS_GENERIC: Platform.COVER,  # widgetName, uiClass is Generic (not supported)
-    UIClass.SCREEN: Platform.COVER,
-    UIClass.SHUTTER: Platform.COVER,
-    UIClass.SIREN: Platform.SIREN,
-    UIWidget.SIREN_STATUS: None,  # widgetName, uiClass is Siren (switch)
+    UIWidget.SIREN_STATUS: None,  # widgetName, uiClass is Siren (siren)
     UIWidget.SOMFY_THERMOSTAT: Platform.CLIMATE,  # widgetName, uiClass is HeatingSystem (not supported)
     UIWidget.STATELESS_EXTERIOR_HEATING: Platform.CLIMATE,  # widgetName, uiClass is ExteriorHeatingSystem.
-    UIClass.SWIMMING_POOL: Platform.SWITCH,
-    UIClass.SWINGING_SHUTTER: Platform.COVER,
-    UIClass.VENETIAN_BLIND: Platform.COVER,
-    UIClass.WINDOW: Platform.COVER,
 }
 
 # Map Overkiz camelCase to Home Assistant snake_case
