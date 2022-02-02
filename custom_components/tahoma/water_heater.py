1ce7c0268666f26d7d2f0df47f34122085eef522
Rodolphe Guillard <rguillard77@gmail.com>
2022-02-02 10:40:07 +0100

    fix: make code aligned with linters requirements

--- custom_components/tahoma/water_heater.py
+++ custom_components/tahoma/water_heater.py
@@ -3,7 +3,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from pyoverkiz.enums import UIWidget
+from pyoverkiz.enums import OverkizState, UIWidget
 
 from .const import DOMAIN
 from .water_heater_devices.domestic_hot_water_production import (
@@ -13,17 +13,22 @@ from .water_heater_devices.domestic_hot_water_production_sauter_guelma import (
     DomesticHotWaterProductionSauterGUELMA,
 )
 from .water_heater_devices.hitachi_dhw import HitachiDHW
-from pyoverkiz.enums import OverkizState
 
 TYPE = {
     UIWidget.DOMESTIC_HOT_WATER_PRODUCTION: DomesticHotWaterProduction,
     UIWidget.HITACHI_DHW: HitachiDHW,
-    UIWidget.DOMESTIC_HOT_WATER_PRODUCTION + "SauterGUELMA": DomesticHotWaterProductionSauterGUELMA
+    UIWidget.DOMESTIC_HOT_WATER_PRODUCTION
+    + "SauterGUELMA": DomesticHotWaterProductionSauterGUELMA
 }
 
 def device_type(device):
-    device_full_name = device.widget + device.states.get(OverkizState.CORE_MANUFACTURER_NAME).value + device.states.get(OverkizState.CORE_NAME).value
+    device_full_name = (
+        device.widget
+        + device.states.get(OverkizState.CORE_MANUFACTURER_NAME).value
+        + device.states.get(OverkizState.CORE_NAME).value
+    )
     return device_full_name if device_full_name in TYPE else device.widget
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
--- custom_components/tahoma/water_heater_devices/domestic_hot_water_production_sauter_guelma.py
+++ custom_components/tahoma/water_heater_devices/domestic_hot_water_production_sauter_guelma.py
@@ -1,19 +1,15 @@
 
 """Support for DomesticHotWaterProduction."""
-import logging
-from homeassistant.components.climate.const import SUPPORT_TARGET_TEMPERATURE
+
 from homeassistant.components.water_heater import (
     STATE_ECO,
     SUPPORT_AWAY_MODE,
     SUPPORT_OPERATION_MODE,
-    WaterHeaterEntity,
 )
-from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
+from homeassistant.const import TEMP_CELSIUS
 from pyoverkiz.enums import OverkizCommand, OverkizCommandParam, OverkizState
 
-from .domestic_hot_water_production import (
-    DomesticHotWaterProduction
-)
+from .domestic_hot_water_production import DomesticHotWaterProduction
 
 STATE_MANUAL = "manual"
 STATE_AUTO = "auto"
@@ -21,22 +17,22 @@ STATE_AUTO = "auto"
 OVERKIZ_TO_OPERATION_MODE = {
     OverkizCommandParam.MANUAL_ECO_ACTIVE: STATE_ECO,
     OverkizCommandParam.MANUAL_ECO_INACTIVE: STATE_MANUAL,
-    OverkizCommandParam.AUTO_MODE: STATE_AUTO
+    OverkizCommandParam.AUTO_MODE: STATE_AUTO,
 }
 
 OPERATION_MODE_TO_OVERKIZ = {v: k for k, v in OVERKIZ_TO_OPERATION_MODE.items()}
+
 
 class DomesticHotWaterProductionSauterGUELMA(DomesticHotWaterProduction):
     """Representation of a DomesticHotWaterProductionSauterGUELMA Water Heater."""
 
     _attr_operation_list = [*OPERATION_MODE_TO_OVERKIZ]
 
-    _attr_supported_features = (
-            SUPPORT_OPERATION_MODE | SUPPORT_AWAY_MODE
-        )
+    _attr_supported_features = SUPPORT_OPERATION_MODE | SUPPORT_AWAY_MODE
     _attr_temperature_unit = TEMP_CELSIUS
 
     def __init__(self, device_url, coordinator):
+        """Initialize DomesticHotWaterProductionSauterGUELMA"""
         super().__init__(device_url, coordinator)
 
     @property
@@ -49,15 +45,17 @@ class DomesticHotWaterProductionSauterGUELMA(DomesticHotWaterProduction):
     @property
     def current_temperature(self):
         """Return the current temperature."""
-        return self.executor.select_state(OverkizState.MODBUSLINK_MIDDLE_WATER_TEMPERATURE)
+        return self.executor.select_state(
+            OverkizState.MODBUSLINK_MIDDLE_WATER_TEMPERATURE
+        )
 
     @property
     def is_away_mode_on(self):
         """Return true if away mode is on."""
         return (
             self.executor.select_state(OverkizState.MODBUSLINK_DHW_ABSENCE_MODE)
-                == OverkizCommandParam.ON
-            )
+            == OverkizCommandParam.ON
+        )
 
     async def async_turn_away_mode_on(self):
         """Turn away mode on."""
