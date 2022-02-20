652f6810dbc77b239762607f141f5855a42eac69
Mick Vleeshouwer <mick@imick.nl>
2022-02-20 22:28:52 +0100

    Add extra controller

--- custom_components/tahoma/alarm_entities/__init__.py
+++ custom_components/tahoma/alarm_entities/__init__.py
@@ -1,8 +1,12 @@
 """Alarm Control Panel entities."""
 from pyoverkiz.enums.ui import UIWidget
 
+from .alarm_panel_controller import AlarmPanelController
+from .stateful_alarm_controller import StatefulAlarmController
 from .tsk_alarm_controller import TSKAlarmController
 
 WIDGET_TO_ALARM_ENTITY = {
     UIWidget.TSKALARM_CONTROLLER: TSKAlarmController,
+    UIWidget.STATEFUL_ALARM_CONTROLLER: StatefulAlarmController,
+    UIWidget.ALARM_PANEL_CONTROLLER: AlarmPanelController,
 }
