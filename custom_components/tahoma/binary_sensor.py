92d0f081f795089ef2982c74183fd9198303e61f
Mick Vleeshouwer <mick@imick.nl>
2022-02-20 22:51:43 +0100

    Add missing descriptions

--- custom_components/tahoma/binary_sensor.py
+++ custom_components/tahoma/binary_sensor.py
@@ -103,6 +103,18 @@ BINARY_SENSOR_DESCRIPTIONS: list[OverkizBinarySensorDescription] = [
     ),
     # DomesticHotWaterProduction/WaterHeatingSystem
     OverkizBinarySensorDescription(
+        key=OverkizState.IO_DHW_BOOST_MODE,
+        name="Boost Mode",
+        icon="hass:water-boiler-alert",
+        is_on=lambda state: state == OverkizCommandParam.ON,
+    ),
+    OverkizBinarySensorDescription(
+        key=OverkizState.IO_DHW_ABSENCE_MODE,
+        name="Away Mode",
+        icon="hass:water-boiler-off",
+        is_on=lambda state: state == OverkizCommandParam.ON,
+    ),
+    OverkizBinarySensorDescription(
         key=OverkizState.IO_OPERATING_MODE_CAPABILITIES,
         name="Energy Demand Status",
         device_class=BinarySensorDeviceClass.HEAT,
