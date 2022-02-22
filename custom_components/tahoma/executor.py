f5259c675c446de2d4c1b97975ada25b35fccaef
Mick Vleeshouwer <mick@imick.nl>
2022-02-22 23:32:01 +0100

    Improvements

--- custom_components/tahoma/executor.py
+++ custom_components/tahoma/executor.py
@@ -15,10 +15,10 @@ from .coordinator import OverkizDataUpdateCoordinator
 # the delay to another value
 COMMANDS_WITHOUT_DELAY = [
     OverkizCommand.IDENTIFY,
-    "test",
+    OverkizCommand.TEST,
     OverkizCommand.ON,
     OverkizCommand.OFF,
-    "onWithTimer",
+    OverkizCommand.ON_WITH_TIMER,
 ]
 
 
@@ -73,13 +73,14 @@ class OverkizExecutor:
 
     async def async_execute_command(self, command_name: str, *args: Any) -> None:
         """Execute device command in async context."""
-        # Set the execution duration to 0 seconds for RTS devices without other args
+        # Set the execution duration to 0 seconds for RTS devices on supported commands
         # Default execution duration is 30 seconds and will block consecutive commands
         if (
             self.device.protocol == Protocol.RTS
             and command_name not in COMMANDS_WITHOUT_DELAY
         ):
             args = args + (0,)
+
         try:
             exec_id = await self.coordinator.client.execute_command(
                 self.device.device_url,
